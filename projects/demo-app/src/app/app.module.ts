4d2ee877d912fc8236e2e0da5e3e62246eb5a86a
Kit Ngai <kit.ngai@ones.software>
2024-08-20 13:38:28 +0800

    update project structure

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -3,23 +3,16 @@ import { NgModule } from '@angular/core';
 import { KtdAppComponent } from './app.component';
 import { KtdAppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         KtdAppComponent
     ],
-    imports: [
-        BrowserModule,
+    bootstrap: [KtdAppComponent], imports: [BrowserModule,
         BrowserAnimationsModule,
         KtdAppRoutingModule,
-        HttpClientModule,
         MatIconModule,
-        MatButtonModule
-    ],
-    providers: [],
-    bootstrap: [KtdAppComponent]
-})
+        MatButtonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class KtdAppModule {}
